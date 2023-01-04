57ba42711b30e939d2fdf487abe2742128108006
evan-dimartinis <60354368+evan-dimartinis@users.noreply.github.com>
2023-01-03 21:45:38 -0500

    UI start with dummy auth

--- frontend/gpt-frontend/src/screens/dashboard.js
+++ frontend/gpt-frontend/src/screens/dashboard.js
@@ -1,9 +1,34 @@
 import React from "react";
+import '../styles/dashboard.css'
 
 const Dashboard = (props) => {
     return (
-        <div>
-            <h1>Ayo this is the dashboard page</h1>
+        <div className="dashboard-page-container">
+            <div className="date-container">
+                <h1>{'<'}</h1>
+                <h1>January 3, 2023</h1>
+                <h1>{'>'}</h1>
+            </div>
+            <div className="link-bar">
+                <div className="link-bar-scroll">
+                    <div className="link-element">
+                        <a href="https://worldle.teuteuf.fr/" target="_blank">Worldle</a>
+                    </div>
+                    <div className="link-element">
+                        <a href="https://worldle.teuteuf.fr/" target="_blank">Worldle</a>
+                    </div>
+                    <div className="link-element">
+                        <a href="https://worldle.teuteuf.fr/" target="_blank">Worldle</a>
+                    </div>
+                    <div className="link-element">
+                        <a href="https://worldle.teuteuf.fr/" target="_blank">Worldle</a>
+                    </div>
+                    <div className="link-element">
+                        <a href="https://worldle.teuteuf.fr/" target="_blank">Worldle</a>
+                    </div>
+                </div>
+                <button className="edit-link-btn">Edit</button>
+            </div>
         </div>
     )
 }
