87379f4e3d138f9c94d46213fba0163b656affca
evan-dimartinis <60354368+evan-dimartinis@users.noreply.github.com>
2023-01-04 21:28:03 -0500

    great work on basic auth functions from scratch

--- Server/Queries/auth.py
+++ Server/Queries/auth.py
@@ -15,84 +15,70 @@ class AuthDB():
                 port=2000
             )
     
+    def sign_up(self, username, password):
+        sql = "SELECT hmy from gptuser where username = %s"
+        record = (username,)
+        conn = None
+        token = ''
+        try:
+            cur = self.conn.cursor()
+            cur.execute(sql, record)
+            results = cur.fetchall()
+            if len(results) > 0:
+                raise Exception('Username already exists')
+            else:
+                self.insert_user(username, password)
+                return self.log_in_user(username, password)
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
 
     def insert_user(self, username, password):
         sql = "INSERT INTO gptuser (username, password) VALUES (%s, %s)"
         record = (username, password)
-        conn = None
         try:
             # create a cursor
             cur = self.conn.cursor()
             # execute the INSERT statement
             cur.execute(sql, record)
             # commit the changes to the database
-            conn.commit()
+            self.conn.commit()
             # close the communication with the PostgreSQL database
             cur.close()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
-        finally:
-            if conn is not None:
-                conn.close()
 
     def log_in_user(self, username, password):
-        sql = "SELECT session_token, expiration_dt from gptuser where username = %s and password = %s"
+        sql = "SELECT hmy, session_token, expiration_dt from gptuser where username = %s and password = %s"
         record = (username, password)
-        conn = None
+        token = ''
         try:
             cur = self.conn.cursor()
             cur.execute(sql, record)
             results = cur.fetchall()
+            cur.close()
+            print(results)
             if len(results) < 1:
                 raise Exception('Username or password are incorrect. Please try again')
             else:
-                # 0 = session_token, 1 = expiration_dt
-                # FIGURE OUT THIS LOGIC BECAUSE THIS IS DENSE RN
-                print(results)
-                if results[0][1] is None or results[0][1] < datetime.now(pytz.utc):
-                    print("no expiration token")
-                    #GENERATE SESSION TOKEN AND INSERT INTO USER RECORD
-                    token = secrets.token_urlsafe()
-                    print(token)
-            cur.close()
+                #REGARDLESS WE'RE UPDATING THE TOKEN AND EXPIRATION DATE
+                token = secrets.token_urlsafe()
+                self.update_user_token(results[0][0], token)
+                self.conn.close()
+                return token
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
-        finally:
-            if conn is not None:
-                conn.close()
-        return
+        return token
 
     def update_user_token(self, hmy, token):
-        sql = "UPDATE gptuser set token = %s expiration_dt = current_timestamp + interval '1 week' where hmy = %s"
+        sql = "UPDATE gptuser set session_token = %s, expiration_dt = current_timestamp + interval '1 week' where hmy = %s"
         record = (token, hmy)
-        conn = None
         try:
-            conn = psycopg2.connect(
-                host="localhost",
-                database="GPTProject",
-                user="postgres",
-                password="Sinitramid19",
-                port=2000
-            )
-            cur = conn.cursor()
+            cur = self.conn.cursor()
             cur.execute(sql, record)
-            results = cur.fetchall()
-            if len(results) < 1:
-                raise Exception('Username or password are incorrect. Please try again')
-            else:
-                # 0 = session_token, 1 = expiration_dt
-                if results[0][1] is None or results[0][1] < datetime.now(pytz.utc):
-                    print("no expiration token")
-                    #GENERATE SESSION TOKEN AND INSERT INTO USER RECORD
-                    token = secrets.token_urlsafe()
-                    print(token)
+            self.conn.commit()
             cur.close()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
-        finally:
-            if conn is not None:
-                conn.close()
-        return
 
 a = AuthDB()
-a.log_in_user('evandimar', 'password')
+print(a.sign_up('ev', 'password'))
